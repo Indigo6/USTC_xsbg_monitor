b1a0e2cf3e44a64c9b060b2c84a659896fa3890f
Indigo6 <1401322857@qq.com>
2021-11-19 15:18:40 +0800

    update mail.py

--- lib/mail.py
+++ lib/mail.py
@@ -10,7 +10,7 @@ def send_email(mail_content, mail_server, mail_address, mail_passwd) :
     try:
         message = MIMEText(mail_content, 'plain', 'utf-8')
         message['From'] = Header("学术报告脚本", 'utf-8')   # 发送者
-        message['To'] = Header("学术报告选课通知", 'utf-8')     # 接收者
+        message['To'] = Header("学术报告选课通知", 'utf-8')  # 接收者
         message['Subject'] = Header('学术报告选课通知', 'utf-8')
 
         mail_hosts = {
@@ -18,6 +18,7 @@ def send_email(mail_content, mail_server, mail_address, mail_passwd) :
             "ustc": "mail.ustc.edu.cn"
         }
         mail_host = mail_hosts[mail_server]
+
         server = smtplib.SMTP_SSL(mail_host, 465)
         server.login(mail_address, mail_passwd)
 
@@ -29,7 +30,7 @@ def send_email(mail_content, mail_server, mail_address, mail_passwd) :
 
 
 if __name__ == "__main__":
-    with open('cfg/config.json', encoding='utf-8') as f:
+    with open('cfg/mine.json', encoding='utf-8') as f:
         js = json.load(f)
     mail_server = js['mail_server']
     mail_address = js["mail_address"]
